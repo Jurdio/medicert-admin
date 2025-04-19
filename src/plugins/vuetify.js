bef74a4c84d5e575416042a4d918892fc88576cb
kupets <kupets@evergreens.in.ua>
2025-04-19 20:30:01 +0300

    fix: fix vutify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,14 @@
-// src/plugins/vuetify.js
 import { createVuetify } from 'vuetify'
 import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-// Створюємо Vuetify інстанс
 export default createVuetify({
     icons: {
         defaultSet: 'mdi',
     },
-    components,
+    components: {
+        ...components,
+    },
     directives,
 })
