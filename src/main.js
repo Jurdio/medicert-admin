cee843b14379b03e4aaffdfa8d73b47fd9af7145
kupets <kupets@evergreens.in.ua>
2025-05-21 23:22:00 +0300

    BREAKING CHANGES: add prime vue

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-import router from './router';
+// main.js
 import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import 'vuetify/styles'
-import VueTelInput from 'vue-tel-input';
-import 'vue-tel-input/vue-tel-input.css';
+import App        from './App.vue'
+import router     from './router'
+
+/* PrimeVue */
+import PrimeVue   from 'primevue/config'
+import Ripple     from 'primevue/ripple'       // директива ripple
+import StyleClass from 'primevue/styleclass'   // директива styleclass
+
+/* CSS */
+import 'primevue/resources/themes/saga-blue/theme.css' // можеш змінити тему
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'              // ⬅️ УТИЛІТИ flex/spacing
 
 const app = createApp(App)
 
-app.use(vuetify);
-app.use(VueTelInput);
-app.use(router);
+app.use(router)
+app.use(PrimeVue, { ripple: true })           // увімкнули ripple глобально
 
+/* Директиви, які потрібні для демо-сайдбару */
+app.directive('ripple', Ripple)
+app.directive('styleclass', StyleClass)
 
 app.mount('#app')
