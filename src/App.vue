3844d142bca6e2b795ac45f3c9f639b647f2f660
kupets <kupets@evergreens.in.ua>
2025-05-22 22:49:44 +0300

    fix fix bug with styles

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <Toast />
-
-  <!-- Фіксований сайдбар (нічого не міняємо) -->
-  <div>
-    <Button icon="pi pi-bars" @click="openSidebar" label="Menu" />
-    <SideMenu ref="sidebarRef" />
-  </div>
-
-<!--  &lt;!&ndash; Контент — відсуваємо на ширину бару &ndash;&gt;-->
-<!--  <div class="p-4" style="margin-left:260px">-->
-<!--    <router-view />-->
-<!--  </div>-->
+  <SidebarLayout>
+    <router-view />
+  </SidebarLayout>
 </template>
 
 <script setup>
-import SideMenu from '@/components/SideMenu.vue'
-import {ref} from 'vue'
-
-const sidebarRef = ref()
-
-const openSidebar = () => {
-  sidebarRef.value.open()
-}
+import SidebarLayout from "@/components/SidebarLayout.vue";
 </script>
+
+<style scoped>
+body {
+  margin: 0 !important;
+  padding: 0;
+  overflow: hidden;
+}
+
+</style>
